55a34ce4b8c104ebb6d80d15bd766a121402e538
Zali <48194062+Zalsburg@users.noreply.github.com>
2019-05-02 18:12:07 +1000

    Task 2

--- Practice Rock Paper Scissors/Practice Rock Paper Scissors/Program.cs
+++ Practice Rock Paper Scissors/Practice Rock Paper Scissors/Program.cs
@@ -14,6 +14,7 @@ namespace Practice_Rock_Paper_Scissors
             //Setting up list
             var rps = new List<string> {"rock", "paper", "scissors"};
             var winner = new List<string>();
+            var roundwinner = new List<string>();
             
             //Introducing user to the game
             Console.WriteLine("Hello there");
@@ -21,77 +22,107 @@ namespace Practice_Rock_Paper_Scissors
             Console.WriteLine("Best of three for three rounds... Let's see who will win: Person or Machine");
             Console.WriteLine();
 
-
             //making the round
-            int usercount = 0;
-            int compcount = 0;
-            while (usercount < 2 && compcount < 2) {
-                //randomising computer's response (Reference 1)
-                var random = new Random();
-                int index = random.Next(rps.Count);
-
-                Console.WriteLine("Are you ready?");
-                Console.WriteLine("Paper... Scissors... Rock!");
-                Console.Write("Your turn: ");
-                string useranswer = Console.ReadLine();
-                Console.WriteLine("I say... " + rps[index]);
-                Console.WriteLine();
-                
-                //calculating who wins
-
-                //if the answer is the same:
-                if (useranswer == rps[index])
-                {
-                    Console.WriteLine("Whoops! Try again!");
+            int userroundcount = 0;
+            int comproundcount = 0;
+            while (userroundcount < 2 && comproundcount < 2) {
+                int usercount = 0;
+                int compcount = 0;
+                if (winner.Count > 0) { 
+                    winner.Clear();
                 }
-                //if the answers are not the same:
+                while (usercount < 2 && compcount < 2) {
+                    //randomising computer's response (Reference 1)
+                    var random = new Random();
+                    int index = random.Next(rps.Count);
+
+                    Console.WriteLine("Are you ready?");
+                    Console.WriteLine("Paper... Scissors... Rock!");
+                    Console.Write("Your turn: ");
+                    string useranswer = Console.ReadLine().ToLower();
+                    Console.WriteLine("I say... " + rps[index]);
+                    Console.WriteLine();
+
+                    //calculating who wins
+
+                    //if the answer is the same:
+                    if (useranswer == rps[index]) {
+                        Console.WriteLine("Whoops! Try again!");
+                    }
+                    //if the answers are not the same:
+                    else {
+                        //if the user answers rock...
+                        if (useranswer == "rock" && rps[index] == "scissors") {
+                            Console.WriteLine("You win!");
+                            winner.Add("user");
+                            usercount++;
+                        }
+                        else if (useranswer == "rock" && rps[index] == "paper") {
+                            Console.WriteLine("I win!");
+                            winner.Add("computer");
+                            compcount++;
+                        }
+                        else if (useranswer == "scissors" && rps[index] == "rock") {
+                            Console.WriteLine("I win!");
+                            winner.Add("computer");
+                            compcount++;
+                        }
+                        else if (useranswer == "scissors" && rps[index] == "paper") {
+                            Console.WriteLine("You win!");
+                            winner.Add("user");
+                            usercount++;
+                        }
+                        else if (useranswer == "paper" && rps[index] == "rock") {
+                            Console.WriteLine("You win!");
+                            winner.Add("user");
+                            usercount++;
+                        }
+                        else if (useranswer == "paper" && rps[index] == "scissors") {
+                            Console.WriteLine("I win!");
+                            winner.Add("computer");
+                            compcount++;
+                        }
+                        else { 
+                            Console.WriteLine("Please enter a valid answer");    
+                        }
+                    }
+
+                    Console.WriteLine();
+                    Console.WriteLine();
+                }
+                Console.WriteLine("Who won?");
+                winner.ForEach(Console.WriteLine);
+                Console.WriteLine();
+                if (usercount > compcount) {
+                    Console.WriteLine("You won the round!");
+                    roundwinner.Add("user");
+                    userroundcount++;
+                }
                 else {
-                    //if the user answers rock...
-                    if (useranswer == "rock" && rps[index] == "scissors") {
-                        Console.WriteLine("You win!");
-                        winner.Add("user");
-                        usercount++;
-                    }
-                    else if (useranswer == "rock" && rps[index] == "paper") {
-                        Console.WriteLine("I win!");
-                        winner.Add("computer");
-                        compcount++;
-                    }
-                    else if (useranswer == "scissors" && rps[index] == "rock") {
-                        Console.WriteLine("I win!");
-                        winner.Add("computer");
-                        compcount++;
-                    }
-                    else if (useranswer == "scissors" && rps[index] == "paper") {
-                        Console.WriteLine("You win!");
-                        winner.Add("user");
-                        usercount++;
-                    }
-                    else if (useranswer == "paper" && rps[index] == "rock") {
-                        Console.WriteLine("You win!");
-                        winner.Add("user");
-                        usercount++;
-                    }
-                    else if (useranswer == "paper" && rps[index] == "scissors") {
-                        Console.WriteLine("I win!");
-                        winner.Add("computer");
-                        compcount++;
-                    }
+                    Console.WriteLine("I won the round!");
+                    roundwinner.Add("computer");
+                    comproundcount++;
                 }
 
                 Console.WriteLine();
                 Console.WriteLine();
             }
 
-            Console.WriteLine("Who won?");
-            winner.ForEach(Console.WriteLine);
-            Console.WriteLine();
-            if (usercount > compcount) {
-                Console.WriteLine("You won the round!");
+            Console.WriteLine("So... who prevailed?");
+            Console.WriteLine("Round 1 winner: " + roundwinner[0]);
+            Console.WriteLine("Round 2 winner: " + roundwinner[1]);
+            if (roundwinner.Count > 2) {
+                Console.WriteLine("Round 3 winner: " + roundwinner[2]);
             }
-            else {
-                Console.WriteLine("I won the round!");
+
+            if (userroundcount > comproundcount) {
+                Console.WriteLine("Looks like I need to update my software");
             }
+            else { 
+                Console.WriteLine("The machine is superior!");
+            }
+
+            
 
             Console.ReadKey(true);
 
